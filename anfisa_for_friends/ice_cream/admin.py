bdd019f584a8c56156a652fe840329cf04f21106
Кирилл Пахомов <sasha.sergeev.1992@inbox.ru>
2026-01-12 10:05:29 +1000

    modified:   anfisa_for_friends/anfisa_for_friends/settings.py 	modified:   anfisa_for_friends/anfisa_for_friends/urls.py 	modified:   anfisa_for_friends/core/models.py 	new file:   anfisa_for_friends/db.json 	modified:   anfisa_for_friends/homepage/views.py 	new file:   anfisa_for_friends/ice_cream.json 	modified:   anfisa_for_friends/ice_cream/admin.py 	modified:   anfisa_for_friends/ice_cream/apps.py 	new file:   anfisa_for_friends/ice_cream/migrations/0001_initial.py 	modified:   anfisa_for_friends/ice_cream/models.py 	modified:   anfisa_for_friends/ice_cream/views.py 	new file:   anfisa_for_friends/indented_db.json 	modified:   anfisa_for_friends/templates/homepage/index.html 	modified:   anfisa_for_friends/templates/ice_cream/detail.html 	new file:   anfisa_for_friends/without_ice_cream_icecream.json 	modified:   requirements.txt

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,43 @@
+# ice_cream/admin.py
 from django.contrib import admin
+from .models import Category, IceCream, Topping, Wrapper
 
-# Register your models here.
+admin.site.empty_value_display = 'Не задано'
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )    
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+    empty_value_display = 'Не задано'
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',        
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
